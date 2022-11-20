a4bf3c5258c3789215e83a1f9147e186b971c0f0
RhonaJoyKe <rhonajoy8@gmail.com>
2022-11-20 09:01:50 -0500

    commit tree 019fc6072b20af2f0187e4d0d918751a4863d277

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+    validates :title,presence:true
+    validates :content,length:{minimum:250}
+    validates :summary,length:{maximum:250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]}
+    validate :title_is_clickbait
+    def title_is_clickbait
+        
+    end
+end
 end
